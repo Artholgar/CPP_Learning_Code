46e4af2392907c84b9b6c11ed8ab502ed0fed1ea
Artholgar <arthurpairaud@gmail.com>
2023-01-14 21:49:35 +0100

    completting rectangle

--- chap-02/4-rectangle/Rectangle.h
+++ chap-02/4-rectangle/Rectangle.h
@@ -3,21 +3,19 @@
 class Rectangle
 {
 public:
-    Rectangle(float length, float width)
-        : _length { length }
-        , _width { width }
-    {}
+    Rectangle(float length, float width);
+    Rectangle(float size);
+    Rectangle();
+
+    static void set_default_size(float size);
 
     float get_length() const { return _length; }
     float get_width() const { return _width; }
 
-    void scale(float ratio)
-    {
-        _length *= ratio;
-        _width *= ratio;
-    }
+    void scale(float ratio);
 
 private:
-    float _length;
-    float _width;
+    float        _length;
+    float        _width;
+    static float _default_size;
 };
--- chap-02/4-rectangle/main.cpp
+++ chap-02/4-rectangle/main.cpp
@@ -10,5 +10,16 @@ int main()
     rect.scale(3);
     std::cout << "{ L: " << rect.get_length() << ", W: " << rect.get_width() << " }" << std::endl;
 
+    Rectangle square(2.5f);
+
+    Rectangle::set_default_size(2.f);
+    Rectangle s1; // -> size is 2.f
+    Rectangle s2; // -> size is 2.f
+
+    Rectangle::set_default_size(7.f);
+    Rectangle s3; // -> size is 7.f
+    Rectangle s4; // -> size is 7.f
+    Rectangle s5; // -> size is 7.f
+
     return 0;
 }
