e184b1faf953cd0fe8a2257a94f33b791503431e
Artholgar <arthurpairaud@gmail.com>
2023-01-19 19:37:10 +0100

    doing exo1 and 2

--- chap-05/1-vectors.cpp
+++ chap-05/1-vectors.cpp
@@ -1,4 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 int main()
 {
+    vector<int> braces { 4, 0 };
+
+    vector<int> parenthesis(4, 0);
+
+    int array[] = { 0, 1, 2, 3, 4, 5 };
+
+    vector<int> part(&array[2], &array[5]);
+
+    vector<int> full(array, array + 6);
+
+    for (size_t left = 0, right = full.size() - 1; left < right; left++, right--)
+    {
+        swap(full[left], full[right]);
+    }
+
+    for (auto it = full.begin(); it != full.end(); it += 2)
+    {
+        it = full.emplace(it, *it);
+    }
+
+    while (!full.empty())
+    {
+        full.pop_back();
+    }
+
+    vector<int> v { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+    v.erase(v.begin() + 2, v.begin() + 7);
+
+    auto it = find(v.begin(), v.end(), 9);
+
+    if (it != v.end())
+    {
+        v.erase(it);
+    }
+
+    v.assign(5, 2);
+    v.assign({ 0, 2, 32, -4, 3 });
+    v.clear();
+
     return 0;
 }
--- chap-05/2-sequentials.cpp
+++ chap-05/2-sequentials.cpp
@@ -1,4 +1,11 @@
+#include <array>
+#include <iostream>
 #include <list>
+#include <stack>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 enum Fruit
 {
@@ -12,20 +19,44 @@ enum Fruit
 
 void try_arrays()
 {
-    // Implement array tests here.
+    array<string, Fruit_Count> fruit_names;
+    fruit_names[Apricot]   = "Apricot";
+    fruit_names[Cherry]    = "Cherry";
+    fruit_names[Mango]     = "Mango";
+    fruit_names[Raspberry] = "Raspberry";
 }
 
 void try_lists()
 {
-    std::list<int> l1;
-    std::list<int> l2;
+    std::list<int> l1 { -2, -3, 7, 200, -8, 6 };
+    std::list<int> l2(3, -45);
+
     std::list<int> l3;
+    l3.assign({ 41, 6, 12 });
+
     std::list<int> l4;
+    l4.emplace_front(0);
+    l4.emplace_front(4);
+    l4.emplace_front(6);
+
+    l1.sort();
+    l2.sort();
+    l1.merge(l2);
+
+    l3.splice(l3.begin(), l4);
+    l3.sort();
+
+    auto mid = l1.begin();
+    advance(mid, l1.size() / 2);
+    l1.splice(mid, l3);
 }
 
 void try_stacks()
 {
-    // Implement stack tests here.
+    std::stack<int, std::vector<int>> s1;
+    s1.emplace(0);
+    s1.emplace(1);
+    s1.emplace(2);
 }
 
 int main()
