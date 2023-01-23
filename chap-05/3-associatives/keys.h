767c06b6fdd7a93dce9117701a7275a7b4665d1f
Artholgar <arthurpairaud@gmail.com>
2023-01-23 18:01:54 +0100

    doing exo 3

--- chap-05/3-associatives/keys.h
+++ chap-05/3-associatives/keys.h
@@ -10,6 +10,22 @@ public:
         , _species { species }
     {}
 
+    bool operator<(const ComparableDog& other) const
+    {
+        if (_name < other._name)
+        {
+            return true;
+        }
+        else if (other._name < _name)
+        {
+            return false;
+        }
+        else
+        {
+            return _species < other._species;
+        }
+    }
+
 private:
     std::string _name;
     std::string _species;
@@ -23,7 +39,28 @@ public:
         , _species { species }
     {}
 
+    bool operator==(const HashableDog& other) const
+    {
+        return _name == other._name && _species == other._species;
+    }
+
+    size_t get_hash() const
+    {
+        std::hash<std::string> hash_fct;
+        return hash_fct(_name) + hash_fct(_species);
+    }
+
 private:
     std::string _name;
     std::string _species;
 };
+
+namespace std {
+
+template <>
+struct hash<HashableDog>
+{
+    size_t operator()(const HashableDog& c) const { return c.get_hash(); }
+};
+
+} // namespace std
--- chap-05/3-associatives/main.cpp
+++ chap-05/3-associatives/main.cpp
@@ -1,4 +1,52 @@
+#include "keys.h"
+
+#include <iostream>
+#include <map>
+#include <set>
+#include <unordered_set>
+
+using namespace std;
+
 int main()
 {
+    /*     set<ComparableDog> dogs;
+
+        ComparableDog medor { "medor", "labrador" };
+        dogs.emplace(medor);
+
+        ComparableDog fox { "fox", "dobermann" };
+        dogs.emplace(fox);
+
+        ComparableDog medor_clone = medor;
+        dogs.emplace(medor_clone);
+
+        cout << dogs.size() << endl; */
+
+    /* unordered_set<HashableDog> dogs;
+
+    HashableDog medor { "medor", "Labrador" };
+    dogs.emplace(medor);
+
+    HashableDog fox { "fox", "dobermann" };
+    dogs.emplace(fox);
+
+    HashableDog medor_clone = medor;
+    dogs.emplace(medor_clone);
+
+    cout << dogs.size() << endl; */
+
+    using Owner = std::pair<std::string, std::string>;
+    map<ComparableDog, Owner> owner_by_dogs;
+
+    ComparableDog medor { "medor", "labrador" };
+    ComparableDog gus { "gus", "bordercollie" };
+    ComparableDog zim { "zim", "poodle" };
+    ComparableDog flippy { "flippy", "spaniel" };
+
+    owner_by_dogs.insert({ medor, Owner { "David", "Hing" } });
+    owner_by_dogs.insert_or_assign(gus, Owner { "Leila", "Wallner" });
+    owner_by_dogs.emplace(zim, Owner { "Thomas", "Josephine" });
+    owner_by_dogs.try_emplace(flippy, "Samy", "Attal");
+
     return 0;
 }
