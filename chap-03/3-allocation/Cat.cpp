0703bb0a206eb0e16f1f7897a627f2dccff64d0f
Artholgar <arthurpairaud@gmail.com>
2023-02-07 12:25:39 +0100

    Doing chap 3 exo bag and exo cat

--- chap-03/3-allocation/Cat.cpp
+++ chap-03/3-allocation/Cat.cpp
@@ -1,4 +1,31 @@
+#include "Cat.h"
+
+#include <iostream>
+#include <memory>
+
 int main()
 {
+    auto cat1 = std::make_unique<Cat>("Felix");
+
+    std::cout << *cat1 << std::endl;
+
+    std::unique_ptr<Cat> cat2;
+
+    if (cat2 == nullptr)
+    {
+        std::cout << "Cat 2 is nullptr" << std::endl;
+    }
+
+    cat2 = std::make_unique<Cat>("Uscka");
+
+    std::cout << "cat1 == cat2 ? " << (cat1 == cat2) << std::endl;
+    std::cout << "*cat1 == *cat2 ? " << (*cat1 == *cat2) << std::endl;
+
+    cat1.reset();
+    if (cat1 == nullptr)
+    {
+        std::cout << "cat1 is nullptr." << std::endl;
+    }
+
     return 0;
 }
